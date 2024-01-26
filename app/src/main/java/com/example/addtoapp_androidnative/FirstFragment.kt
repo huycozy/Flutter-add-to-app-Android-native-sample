f115a6489ec841ae4c86d20c19c37ce401542581
Nguyen Huy <huy@nevercode.io>
2024-01-26 12:55:26 +0700

    Use a cached FlutterEngine
    Signed-off-by: Nguyen Huy <huy@nevercode.io>

--- app/src/main/java/com/example/addtoapp_androidnative/FirstFragment.kt
+++ app/src/main/java/com/example/addtoapp_androidnative/FirstFragment.kt
@@ -23,7 +23,7 @@ class FirstFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
 
         _binding = FragmentFirstBinding.inflate(inflater, container, false)
         return binding.root
@@ -39,7 +39,7 @@ class FirstFragment : Fragment() {
 
         binding.openFlutterAct.setOnClickListener {
             startActivity(
-                FlutterActivity.createDefaultIntent(requireActivity())
+                FlutterActivity.withCachedEngine("my_engine_id").build(requireActivity())
             )
         }
     }
