ff364a5af7d00515bc480cc4623d1d12022027fd
Nguyen Huy <huy@nevercode.io>
2024-04-17 11:19:52 +0700

    update
    Signed-off-by: Nguyen Huy <huy@nevercode.io>

--- app/src/main/java/com/example/addtoapp_androidnative/MainActivity.kt
+++ app/src/main/java/com/example/addtoapp_androidnative/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
 import com.example.addtoapp_androidnative.databinding.ActivityMainBinding
+import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -29,8 +30,9 @@ class MainActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
 
         binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+            startActivity(
+                FlutterActivity.withCachedEngine("my_engine_id").build(this)
+            )
         }
     }
 
